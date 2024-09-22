f58d5f618b5f740c7ad29d5ec1fafa93b4eb158d
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-22 21:16:07 +0330

    Implement course and instructor models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = 'ST', _('Student')
+        TEACHER = 'TE', _('Teacher')
+        ADMIN = 'AD', _('Admin')
+    role = models.CharField(max_length=2, choices=Role, default=Role.STUDENT)
+
+    
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
-# Create your models here.
+class Course(models.Model):
+    class Level(models.TextChoices):
+        BEGINNER = 'BE', _('Beginner')
+        INTERMEDIATE = 'IN', _('Intermediate')
+        ADVANCED = 'AD', _('Advanced')
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField()
+    level = models.CharField(max_length=2, choices=Level, default=Level.BEGINNER)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    instructors = models.ManyToManyField(settings.AUTH_USER_MODEL, through='InstructorCourse')
+
+
+# Intermediate Table
+class InstructorCourse(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE) 
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    assigned_date = models.DateField()
+    role = models.CharField(max_length=100)
