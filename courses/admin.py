56b69a93e712ce42787b9efc22a739cfd36ca3d7
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-23 19:28:28 +0330

    Set up a simple admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course, Module, Lesson, Enrollment, Assignment, Submission
 
-# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Lesson)
+admin.site.register(Enrollment)
+admin.site.register(Assignment)
+admin.site.register(Submission)
