328aff9d02f910a03d424922fab034ed2e8d1594
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-24 19:00:35 +0330

    Register course and module in admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Course, Module, Lesson, Enrollment, Assignment, Submission
+from .models import Course, Module, Lesson, Enrollment, Assignment, Submission, InstructorCourse
 
 
-admin.site.register(Course)
-admin.site.register(Module)
-admin.site.register(Lesson)
-admin.site.register(Enrollment)
-admin.site.register(Assignment)
-admin.site.register(Submission)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'level', 'price', 'get_instructors', 'updated_at']
+    list_filter = ['category', 'level', 'price', 'updated_at']
+    list_editable = ['price']
+
+    def get_instructors(self, obj):
+        return "\n".join([ins.username for ins in obj.instructors.all()])
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'course', 'order', 'updated_at']
+    list_filter = ['course', 'order', 'updated_at']
