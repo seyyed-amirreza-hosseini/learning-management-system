4d3ac7eee6ab5c803b6cd9aca02adcf666c8f458
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-24 18:28:40 +0330

    Switch the default database from sqlite to postgresql

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "education_platform.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lms_db',              
+        'USER': 'amirreza',               
+        'PASSWORD': 'seyyedamirreza1381',       
+        'HOST': 'localhost',            
+        'PORT': '5432',           
     }
 }
 
