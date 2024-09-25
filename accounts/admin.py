69871c79aa92adb05a15f67a452da6450055295b
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-25 15:54:00 +0330

    Add items to list display

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import User
 
 
-admin.register(User)
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['']
+    list_display = ['first_name', 'last_name', 'email', 'role', 'is_staff']
