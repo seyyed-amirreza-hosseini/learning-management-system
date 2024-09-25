677fcdcd0ce4993afc1dbbace4b3e8b4b6491b98
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-25 14:39:17 +0330

    Add custom manager to managers.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,7 @@
-from typing import Any
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, password, **extra_fields)
+from .managers import UserManager
 
 
 class User(AbstractUser, PermissionsMixin):
@@ -35,4 +18,3 @@ class User(AbstractUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-  
