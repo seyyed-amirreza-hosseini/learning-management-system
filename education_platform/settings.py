6e6387f347940b5cbcc1245c62f6b1ec237ef991
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-26 15:29:06 +0330

    Install django debug toolbar

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "accounts",
     "courses",
 ]
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "education_platform.urls"
--- education_platform/urls.py
+++ education_platform/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + debug_toolbar_urls()
