6ad0ded704c1f7331e228a33f34d0709be9a59e8
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-27 18:35:54 +0330

    Add a nested serializer to display instructors

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Course
+from .models import Course, Teacher
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField(method_name='get_full_name')
+
+    class Meta:
+        model = Teacher
+        fields = ['id', 'full_name', 'department']
+    
+    def get_full_name(self, obj):
+        return f'{obj.user.first_name} {obj.user.last_name}'
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    instructors = TeacherSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
         fields = ['id', 'name', 'category', 'level', 'price', 'instructors']
