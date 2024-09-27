1073177426c5bcb7382ce726ecd4454f4a269140
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-27 18:42:17 +0330

    Write custom permissions for admin and teacher access

---
+++ courses/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+METHODS = ['POST', 'PUT', 'PATCH', 'DELETE']
+
+
+class IsAdminOrTeacher(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        # Make sure that user is not Anonymouse
+        if request.user.is_authenticated:
+            return bool(
+                request.user and
+                (request.user.is_staff or request.user.role=='TE')
+            )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in METHODS:
+            if request.user.is_authenticated:
+                return bool(
+                    request.user and
+                    (request.user.is_staff or request.user.role=='TE')
+                )
--- courses/views.py
+++ courses/views.py
@@ -4,10 +4,12 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Course
 from .serializers import CourseSerializer
+from .permissions import IsAdminOrTeacher
 
 
 class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrTeacher]
 
     def get_queryset(self):
         return Course.objects.prefetch_related('instructors').all()
