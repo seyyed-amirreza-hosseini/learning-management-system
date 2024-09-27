1e9c49a644c70f7aa14f3a767d3c8450cdb3d625
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-27 11:01:54 +0330

    Install django rest framework

--- education_platform/urls.py
+++ education_platform/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ] + debug_toolbar_urls()
