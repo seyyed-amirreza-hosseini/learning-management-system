4eaa067166589e9788869623e74863f799630e8c
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-01 16:48:53 +0330

    Modify access token lifetime

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,7 +146,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    "AUTH_HEADER_TYPES": ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
 }
 
 DJOSER = {
