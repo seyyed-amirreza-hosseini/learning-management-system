0e5391c5276584eeefe9e9a0d72c2978ba825736
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-01 16:39:02 +0330

    Customize user create serializer of djoser

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -147,3 +147,9 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'accounts.serializers.UserCreateSerializer',
+    },
+}
