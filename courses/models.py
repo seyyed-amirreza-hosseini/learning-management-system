f6b00ee1c0ce0dff35633cb33640c9ef008e90d4
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-03 18:54:40 +0330

    Revisite models

--- courses/models.py
+++ courses/models.py
@@ -104,10 +104,12 @@ class Assignment(models.Model):
     description = models.TextField()
     due_date = models.DateTimeField()
     max_score = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxLengthValidator(20)])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments')
     module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='assignments')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='assignments')
 
     def __str__(self):
-        return f'{self.name}: {self.module.name}'
+        return f'{self.name}: {self.course.name} - {self.module.name} - {self.lesson.name}'
 
 
 class Enrollment(models.Model):
