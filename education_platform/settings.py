cf057cba74c399bf88ac92e627e95ed98b1ded60
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-17 17:55:56 +0330

    Add celery configurations to settings.py file

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -157,3 +157,12 @@ DJOSER = {
         'current_user': 'accounts.serializers.UserSerializer',
     },
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+INSTALLED_APPS += ['django_celery_beat']
