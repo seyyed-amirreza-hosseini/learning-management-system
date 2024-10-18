1a3c7a2ba7571af47a6e5eeaee68031f5c6013b6
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-18 18:30:28 +0330

    Specify celery broker url

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -158,11 +158,9 @@ DJOSER = {
     },
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
-
-INSTALLED_APPS += ['django_celery_beat']
