0f9819e20d907c750f2145d70b1561623ee9ed34
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-24 20:36:16 +0330

    Modify log_user_activity helper function

--- courses/utils.py
+++ courses/utils.py
@@ -1,13 +1,14 @@
 from .models import UserActivityLog
 
 
-def log_user_activity(user, action, course=None):
+def log_user_activity(user, action, course=None, details=None):
     """
     Logs a user activity with optional course and additional.
     """
     UserActivityLog.objects.create(
         user=user,
         action=action,
-        course=course
+        course=course,
+        details=details
     )
     
