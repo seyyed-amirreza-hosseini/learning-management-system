ce3a6adfb79698b103671f6579f03fd4704681a3
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-26 21:15:54 +0330

    Add Celery task to send weekly course completion

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,6 +1,8 @@
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db.models import Avg
 from celery import shared_task
+from courses.models import UserCourseProgress
 
 
 @shared_task
@@ -12,3 +14,10 @@ def send_assignment_reminder_email(subject, message, recipient_list):
         recipient_list,
         fail_silently=False,
     )
+
+@shared_task
+def send_weekly_course_report():
+    average_progress = UserCourseProgress.objects.aggregate(avg_progress=Avg('progress_percentage'))
+    report = f"Average course completion rate: {average_progress['avg_progress']:.2f}%"
+
+    send_mail('Weekly Course Report', report, 'admin@lms.com', ['students@lms.com'])
