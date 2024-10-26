be703131078cdb92e32aaa763626a080e93291c3
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-26 21:14:49 +0330

    Add django_celery_beat to INSTALLED_APPS

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "django_celery_beat",
     "rest_framework",
     "djoser",
     "debug_toolbar",
