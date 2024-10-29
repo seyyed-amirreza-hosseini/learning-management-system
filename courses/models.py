3e3866d3ca040e00d0a9686d04d385fd73ee642c
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-29 18:47:54 +0330

    Revisit Meeting model

--- courses/models.py
+++ courses/models.py
@@ -288,7 +288,6 @@ class Meeting(models.Model):
     host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='meetings')
     topic = models.CharField(max_length=255)
     start_time = models.DateTimeField()
-    meeting_id = models.CharField(max_length=255, unique=True)
     meeting_link = models.URLField()
 
 
