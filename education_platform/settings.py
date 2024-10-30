562cca0749a4e2d2306c56e8c9b87d83d0895e3e
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-10-30 21:03:17 +0330

    Set DEFAULT_SCHEMA_CLASS for REST_FRAMEWORK using drf-spectacular

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf-spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
