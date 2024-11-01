be0dd908e883712aaf096cc7a7dc08eaa71ae270
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-11-01 20:00:33 +0330

    Configure authentication backends for OAuth2 and Django

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -165,6 +165,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+AUTHENTICATION_BACKENDS = (
+   'drf_social_oauth2.backends.DjangoOAuth2',
+   'django.contrib.auth.backends.ModelBackend',
+)
+
 SPECTACULAR_SETTINGS = {
     'TITLE': 'LMS',
 }
