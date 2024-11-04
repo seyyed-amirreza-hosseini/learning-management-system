21a53d4dfba5f4101ac6426741e58ab0754ac654
amirreza <hosseiniseyyedamirreza2003@gmail.com>
2024-11-04 20:48:15 +0330

    Add home and logout pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
+def home(request):
+    return render(request, 'accounts/home.html')
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def logout_view(request):
+    logout(request)
+    return redirect('/')
